c77af3e738ad0a108d46dd9142f9414b97950d5a
Hana <2006yihui@gmail.com>
2026-04-26 01:12:54 +0800

    refactor: remove Google Fonts dependency for static assets and add language selection UI

--- survivai/lib/main.dart
+++ survivai/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  GoogleFonts.config.allowRuntimeFetching = false;
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
